6b68412ceca5cb629604c935280be4d7e83d5936
estevo <pedro.cordova2002@gmail.com>
2023-10-07 23:02:24 -0300

    delete element almost done

--- tree/src/main/java/com/estevao/tree/Arvore.java
+++ tree/src/main/java/com/estevao/tree/Arvore.java
@@ -1,5 +1,7 @@
 package com.estevao.tree;
 
+import javax.swing.text.Element;
+
 public class Arvore<T extends Comparable> {
 
     private Elemento<T> raiz;
@@ -32,6 +34,94 @@ public class Arvore<T extends Comparable> {
                 }
             }
         }
+    }
+
+    public boolean remover(T valor) {
+        // buscar o elemento na árvore
+        Elemento<T> atual = this.raiz;
+        Elemento<T> paiAtual = null;
+        while(atual != null) {
+            if (atual.getValor().equals(valor)) {
+                break;
+            } else if (valor.compareTo(atual.getValor()) < 0) { // valor menor que o valor atual
+                paiAtual = atual;
+                atual = atual.getEsquerda();
+            } else { // valor maior que o valor atual
+                paiAtual = atual;
+                atual = atual.getDireita();
+            }
+        }
+        // verifica se existe o elemento
+        if (atual != null) {
+
+            // elemento tem 2 filhos
+            if (atual.getDireita() != null && atual.getEsquerda() != null) {
+
+            // tem 1 filho à esquerda
+            } else if (atual.getEsquerda() != null) {
+
+                Elemento<T> substituto = atual.getEsquerda();
+                Elemento<T> paiSubstituto = atual;
+
+                // procure o elemento mais à direita do galho à esquerda
+                while(substituto.getDireita() != null) {
+                    paiSubstituto = substituto;
+                    substituto = substituto.getDireita();
+                }
+
+                // coloca o substituto dentro do nó trocado da árvore
+                if (atual.getValor().compareTo(paiAtual.getValor()) < 0) {
+                    paiAtual.setEsquerda(substituto);
+                } else {
+                    paiAtual.setDireita(substituto);
+                }
+
+                //remove o elemento da árvore
+                if (substituto.getValor().compareTo(paiSubstituto.getValor()) < 1) {
+                    paiSubstituto.setEsquerda(null);
+                } else {
+                    paiSubstituto.setDireita(null);
+                }
+
+
+            // tem 1 filho á direita
+            } else if (atual.getDireita() != null) {
+
+                Elemento<T> substituto = atual.getDireita();
+                Elemento<T> paiSubstituto = atual;
+
+                // procure o elemento mais à esquerda do galho à direita
+                while(substituto.getEsquerda() != null) {
+                    paiSubstituto = substituto;
+                    substituto = substituto.getEsquerda();
+                }
+
+                // coloca o substituto dentro do nó trocado da árvore
+                if (atual.getValor().compareTo(paiAtual.getValor()) < 0) {
+                    paiAtual.setEsquerda(substituto);
+                } else {
+                    paiAtual.setDireita(substituto);
+                }
+
+                //remove o elemento da árvore
+                if (substituto.getValor().compareTo(paiSubstituto.getValor()) < 1) {
+                    paiSubstituto.setEsquerda(null);
+                } else {
+                    paiSubstituto.setDireita(null);
+                }
+
+
+            // não tem filhos
+            } else {
+                if (paiAtual.getDireita() != null) {
+                    paiAtual.setDireita(null);
+                } else {
+                    paiAtual.setEsquerda(null);
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     public void excluirElemento(Elemento<T> valor) {
