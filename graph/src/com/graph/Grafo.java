45652b497b54ebdc34eefb26a5cbec1497aaff15
estevo <pedro.cordova2002@gmail.com>
2023-10-08 21:41:07 -0300

    vertices and arestas add

--- graph/src/com/graph/Grafo.java
+++ graph/src/com/graph/Grafo.java
@@ -1,6 +1,26 @@
 package com.graph;
 
+import java.util.ArrayList;
+
 public class Grafo<T> {
+
+    private ArrayList<Vertice<T>> vertices;
+    private ArrayList<Aresta<T>> arestas;
+
+    public Grafo() {
+        this.vertices = new ArrayList<>();
+        this.arestas = new ArrayList<>();
+    }
+
+    public void adicionarVertice(T dado) {
+        // cria vertice e coloca na lista de vertices do grafo
+        Vertice<T> novoVertice = new Vertice<>(dado);
+        this.vertices.add(novoVertice);
+    }
+
+    public void adicionarAresta(Double peso, T inicio, T fim) {
+
+    }
 
 
 }
--- graph/src/com/graph/Vertice.java
+++ graph/src/com/graph/Vertice.java
@@ -1,4 +1,32 @@
 package com.graph;
 
-public class Vertice {
+import java.util.ArrayList;
+
+public class Vertice<T> {
+
+    private T dado;
+    private ArrayList<Aresta<T>> arestasEntrada;
+    private ArrayList<Aresta<T>> arestasSaida;
+
+    public Vertice(T valor) {
+        this.dado = valor;
+        this.arestasEntrada = new ArrayList<>();
+        this.arestasSaida = new ArrayList<>();
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public void adicionarArestaEntrada(Aresta<T> aresta) {
+        this.arestasEntrada.add(aresta);
+    }
+
+    public void adicionarArestaSaída(Aresta<T> aresta) {
+        this.arestasSaida.add(aresta);
+    }
 }
